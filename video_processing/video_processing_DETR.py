32a768ebacb09ca12e35f8b5602628abd3b4eebe
Ana Gomez <anitaggrr@gmail.com>
2023-12-18 14:37:03 +0100

    fix: env variables managed with decouple config

--- video_processing/video_processing_DETR.py
+++ video_processing/video_processing_DETR.py
@@ -2,14 +2,17 @@ import cv2
 from pytube import YouTube
 from transformers import DetrForObjectDetection, DetrImageProcessor
 import supervision as sv
-import matplotlib.pyplot as plt
-from dotenv import load_dotenv
-import os
 import torch
+from decouple import config
 
 
-def get_model_and_image_processor():    
-    MODEL_PATH = os.getenv('MODEL_PATH')
+def get_model_and_image_processor():
+    '''
+    Retrieves the DETR image processor and the model trained with the custom dataset.
+    Returns:
+        Tuple[DetrImageProcessor, DetrForObjectDetection]: A tuple containing the DETR image processor and model.
+    '''    
+    MODEL_PATH = config('MODEL_PATH')
     CHECKPOINT = 'facebook/detr-resnet-50'
 
     image_processor = DetrImageProcessor.from_pretrained(CHECKPOINT)
@@ -18,6 +21,14 @@ def get_model_and_image_processor():
 
 
 def download_video(youtube_url, output_path):
+    '''
+    Downloads a video from YouTube.
+    Args:
+        youtube_url (str): The YouTube video URL.
+        output_path (str): The path to the folder where the video will be saved.
+    Returns:
+        str: The filename of the downloaded video.
+    '''
     yt = YouTube(youtube_url)
     ys = yt.streams.get_highest_resolution()
     ys.download(output_path)
@@ -25,6 +36,15 @@ def download_video(youtube_url, output_path):
 
 
 def process_video_with_model(video_path, image_processor, model):
+    '''
+    Processes a video using a DETR model and displays real-time object detection results.
+    Args:
+        video_path (str): The path to the video file.
+        image_processor: The DETR image processor.
+        model: The pretrained DETR model for object detection.
+    Returns:
+        None
+    '''
     cap = cv2.VideoCapture(video_path)
 
     frame_count = 0
@@ -36,9 +56,10 @@ def process_video_with_model(video_path, image_processor, model):
         if not ret:
             break
 
+        #Skips frames to speed up inference
         frame_count += 1
         if frame_count % (frames_to_skip + 1) != 0:
-            continue  # Salta frames_to_skip frames
+            continue
         
         #Process the frame with DETR and obtain detections
         with torch.no_grad():
@@ -46,14 +67,14 @@ def process_video_with_model(video_path, image_processor, model):
             inputs = image_processor(images=frame, return_tensors='pt')
             outputs = model(**inputs)
             
-            # Post-process
+            #Post-process
             target_sizes = torch.tensor([frame.shape[:2]])
             results = image_processor.post_process_object_detection(outputs=outputs,
                                                                     threshold=0.40,
                                                                     target_sizes=target_sizes)[0]
 
 
-            # Draw the detections on the frame
+            #Draw the detections on the frame
             detections = sv.Detections.from_transformers(transformers_results=results)
             labels = [f"{model.config.id2label[class_id]} {confidence:0.2f}" for _, confidence, class_id, _ in detections]
 
@@ -64,7 +85,7 @@ def process_video_with_model(video_path, image_processor, model):
             #Display the frame with detections
             cv2.imshow('Detected Objects', annotated_frame)
 
-            # Wait for a key press or a short delay
+            #Wait for a key press or a short delay
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
@@ -74,16 +95,12 @@ def process_video_with_model(video_path, image_processor, model):
 
 def main():
 
-    load_dotenv()
-
-    # Especifica la URL de YouTube y la carpeta de salida
     youtube_url = 'https://www.youtube.com/watch?v=G_-B8fPkgb0'
-    output_folder = os.getenv('VIDEO_FOLDER')
 
-    # Descarga el video desde YouTube
+    output_folder = config('VIDEO_FOLDER')
+
     video_filename = download_video(youtube_url, output_folder)
 
-    # Obtiene el path completo del video descargado
     video_path = f"{output_folder}/{video_filename}"
 
     image_processor, model = get_model_and_image_processor()   
--- video_processing/video_processing_YOLO.py
+++ video_processing/video_processing_YOLO.py
@@ -1,10 +1,18 @@
 from ultralytics import YOLO
 import cv2
 from pytube import YouTube
-import matplotlib.pyplot as plt
+from decouple import config
 
 
 def download_video(youtube_url, output_path):
+    '''
+    Downloads a video from YouTube.
+    Args:
+        youtube_url (str): The YouTube video URL.
+        output_path (str): The path to the folder where the video will be saved.
+    Returns:
+        str: The filename of the downloaded video.
+    '''
     yt = YouTube(youtube_url)
     ys = yt.streams.get_highest_resolution()
     ys.download(output_path)
@@ -12,6 +20,16 @@ def download_video(youtube_url, output_path):
 
 
 def process_video_with_model(video_path, model):
+    '''
+    Processes a video using a YOLOv8 model and displays real-time inference results.
+    Args:
+        video_path (str): The path to the video file.
+        model: The YOLOv8 model object.
+    Returns:
+        None
+    This function displays the video frames with the inference results 
+    until the user presses 'q'.
+    '''
     cap = cv2.VideoCapture(video_path)
 
     #Loop through the video frames
@@ -23,42 +41,36 @@ def process_video_with_model(video_path, model):
             #Run YOLOv8 inference on the frame
             results = model(frame)
 
-            # Visualize the results on the frame
+            #Visualize the results on the frame
             annotated_frame = results[0].plot()
 
-            # Display the annotated frame
+            #Display the annotated frame
             cv2.imshow("YOLOv8 Inference", annotated_frame)
 
-            # Break the loop if 'q' is pressed
+            #Break the loop if 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
         else:
-            # Break the loop if the end of the video is reached
+            #Break the loop if the end of the video is reached
             break
 
-    # Release the video capture object and close the display window
+    #Release the video capture object and close the display window
     cap.release()
     cv2.destroyAllWindows()
 
 
 def main():
 
-    # load_dotenv()
-
-    # Especifica la URL de YouTube y la carpeta de salida
     youtube_url = 'https://www.youtube.com/watch?v=IXFFhtD8lhk'
 
-    # output_folder = os.getenv('VIDEO_FOLDER')
-    output_folder = 'C:/Users/Ana Milena GOMEZ/Documents/Ana Milena GOMEZ/IA-School_Factoria-F5/ComputerVision/ReconocimientoObjetos/video_folder/'
-
-    # Descarga el video desde YouTube
+    output_folder = config('VIDEO_FOLDER')
+   
     video_filename = download_video(youtube_url, output_folder)
 
-    # Obtiene el path completo del video descargado
+    #Obtains the full path of the downloaded video
     video_path = f"{output_folder}/{video_filename}"
 
-    # Load the YOLOv8 model
-    model_path = 'C:/Users/Ana Milena GOMEZ/Documents/Ana Milena GOMEZ/IA-School_Factoria-F5/ComputerVision/ReconocimientoObjetos/best_YOLO_model/best.pt'
+    model_path = config('YOLO_PATH')
 
     model = YOLO(model_path)
 
