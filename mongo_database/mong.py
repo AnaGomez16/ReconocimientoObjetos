b0e16791e3b300d8793920b9b2682ef2a8bfc7e3
Ana Gomez <anitaggrr@gmail.com>
2023-12-20 06:43:33 +0100

    feat: data successfully saved in the database

--- mongo_database/mong.py
+++ mongo_database/mong.py
@@ -2,14 +2,19 @@ from flask import Flask, request
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 from decouple import config
+import logging
 
 #MongoDB connection
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/upload_report": {"origins": "*"}})
 app.config['MONGO_URI'] = config('MONGO_URI')
 mongo = PyMongo(app)
 
 print('Connection to MongoDB established successfully.') if mongo else print('Error connecting to MongoDB.')
+
+@app.route('/')
+def index():
+    return 'Hello World!'
 
 @app.route('/upload_report', methods=['POST'])
 def upload_report():
@@ -19,7 +24,7 @@ def upload_report():
     video_and_detections_report = mongo.db.detections.insert_one(data)
 
     if video_and_detections_report:
-        return 'Data successfully saved in MongoDB.B', 200
+        return 'Data successfully saved in MongoDB.', 200
     else:
         return 'Error while saving data.', 500
 
