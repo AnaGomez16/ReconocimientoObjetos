72f4392adb48dc6405617b2075475fc3de65a55e
Ana Gomez <anitaggrr@gmail.com>
2023-12-20 01:20:43 +0100

    feat: video processing through streamlit interface

--- str_app.py
+++ str_app.py
@@ -1,22 +1,36 @@
 import streamlit as st
+from decouple import config
+from video_processing.video_processing_YOLOv8 import download_video, process_video_with_model
+from video_processing.video_processing_YOLOv8 import get_video_report, get_logo_report
+from ultralytics import YOLO
+
+
+#Function to check if the URL is a valid YouTube link
+def valid_youtube_link(url):
+    return "youtube.com" in url or "youtu.be" in url
+
 
 st.title('Brand Logo Detection')
 
-#Function to check if the URL is a valid YouTube link
-def es_enlace_youtube(url):
-    return "youtube.com" in url or "youtu.be" in url
-
-#sección para mostrar el video
-st.subheader('Insertar URL del video')
-video_url = st.text_input('Insertar URL del video')
-
-
-if video_url and ("youtube.com" in video_url or "youtu.be" in video_url):
-        if es_enlace_youtube(video_url):
-         st.success("El enlace es válido.")
-        # Aquí puedes realizar la lógica adicional que desees al tener un enlace de YouTube válido
-else:
-        st.error("El enlace no es válido. Por favor, introduce un enlace válido de YouTube.")
+video_url = st.text_input('Insert video URL')
 
 if video_url:
-    st.video(video_url)
+   if valid_youtube_link(video_url):
+      st.success("The link is valid.")
+      st.video(video_url)
+        
+      output_folder = config('VIDEO_FOLDER')
+      video_filename = download_video(video_url, output_folder)
+      video_path = f"{output_folder}/{video_filename}"
+        
+      model_path = config('YOLO_PATH')
+      model = YOLO(model_path)
+      labels_list = list(model.names.values())
+      detected_labels = process_video_with_model(video_path, model)
+
+      duration, frame_rate, info_dict = get_video_report(video_path, video_filename)
+      info_dict['detections'] = get_logo_report(labels_list, detected_labels, duration, frame_rate)
+      print(info_dict)
+
+   else:
+      st.error("The link is not valid. Please, enter a valid YouTube link.")
