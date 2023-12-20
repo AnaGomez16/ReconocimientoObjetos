fc5b18fc2b0880a1ca8a1e9e1587cfe4db39dbab
Ana Gomez <anitaggrr@gmail.com>
2023-12-20 03:33:24 +0100

    feat: mongo runs locally

--- mongo_database/mong.py
+++ mongo_database/mong.py
@@ -1,34 +1,28 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
-from flask_cors import CORS  
+from flask_cors import CORS
+from decouple import config
 
-#Aqui la conección a mongo
+#MongoDB connection
 app = Flask(__name__)
 CORS(app)
-app.config['MONGO_URI'] = 'mongodb://localhost/imagenes'
+app.config['MONGO_URI'] = config('MONGO_URI')
 mongo = PyMongo(app)
 
-print("Conexión a MongoDB establecida con éxito") if mongo else print("Error al conectarse a MongoDB")
+print('Connection to MongoDB established successfully.') if mongo else print('Error connecting to MongoDB.')
 
+@app.route('/upload_report', methods=['POST'])
+def upload_report():
+    #Data sent from Streamlit is received.
+    data = request.json
 
-@app.route('/upload_videos', methods=['POST'])
-def upload_video():
-    data = request.json  #Se Obtienen los datos enviados desde Streamlit
+    video_and_detections_report = mongo.db.detections.insert_one(data)
 
-    # Procesa los datos y se guardan en MongoDB
-    if data and 'video_url' in data:
-        # Aquí puedes realizar la lógica para procesar los datos del video
-        # y guardar la información relevante en tu base de datos MongoDB
-
-        #guardar la URL del video
-        inserted_video = mongo.db.videos.insert_one({'video_url': data['video_url']})
-            #se agrega la funcion que agrega el diccionario 
-        if inserted_video:
-            return 'Video guardado exitosamente en MongoDB', 200
-        else:
-            return 'Error al guardar el video', 500
+    if video_and_detections_report:
+        return 'Data successfully saved in MongoDB.B', 200
     else:
-        return 'Datos no válidos', 400
+        return 'Error while saving data.', 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
